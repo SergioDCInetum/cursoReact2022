376858a20b35e574dbce03126befb61d495f2d76
SergioDCInetum <villasergi@hotmail.com>
2022-03-08 10:35:16 +0100

    Práctica 2
    Se añade el botón de muestra

--- myProject/App.js
+++ myProject/App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight} from 'react-native';
+
+// al añadir el botón y arrancar la aplicación, da una excepción
+const Button = () => (
+  <TouchableHighlight>
+    <Text>Hello World</Text>
+  </TouchableHighlight>
+)
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Práctica 1 y 2. Crear aplicación con un botón</Text>
+      <Button/>
       <StatusBar style="auto" />
     </View>
   );
